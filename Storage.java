5d93cdf3f097cdf1da5c9278665b6db910418236
JGALVEAB <104497110+JGALVEAB@users.noreply.github.com>
2022-05-26 17:48:30 +0200

    Add files via upload

---
+++ Storage.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Storage {
+	
+	public void storage() {
+	 try (BufferedReader storagedrive = new BufferedReader(new FileReader(memory));){
+		 int counter =0;
+		 String input;
+		 List<String> shiftSchedule = new ArrayList<>();
+		
+		 
+		 while((input = storagedrive.readLine()) != null) {
+			 if(input.isEmpty()) {
+				 
+			 }
+		 }
+		 
+		 
+	 }catch(IOException e) {
+		 e.printStackTrace();
+	 }
+		  
+	  
+  }
+}
