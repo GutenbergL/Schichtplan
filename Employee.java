55a6d70d408bb5bb42d8062e46ef4707207c29d6
JGALVEAB <104497110+JGALVEAB@users.noreply.github.com>
2022-05-26 18:34:42 +0200

    read description

--- Employee.java
+++ Employee.java
@@ -1,4 +1,22 @@
 
 public class Employee {
-	
+
+    String day;
+    double date;
+    double time;
+
+	public Employee(String day, double date, double time){
+        this.day = day;
+        this.date = date;
+        this.time = time;
+    }
+    public String getDay(){
+        return day;
+    }
+    public double getDate(){
+        return date;
+    }
+    public double getTime(){
+        return time;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,71 +1,13 @@
-import java.io.FileReader;
-import java.io.File;
-import java.io.FileWriter;
-import java.util.Scanner;
+
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Press 1 to start");
-		int breakP = 1;
-		Scanner sc = new Scanner(System.in);
-		char userInput = sc.next().charAt(0);
-		do {
-			if (userInput == '1') {
-				// Nur zum testen der Funktion, werden sp�ter sysout mit storagedrive ersetzen
-				System.out.println("");
-				System.out.println("");
-				System.out.println("Was moechten sie tuen ?");
-				System.out.println("Schicht löschen: 1		" + "		Schicht verschieben 2		"
-						+ "		Schicht eintragen 3");
+		//wäre es in Ordnung wenn wir alles auf 
+		//englisch schreiben, weil nach einem Upload auf github werden alle Umlaute falsch interpretiert, einfach um weniger fehlerquellen zu haben?
 
-				char userInput2 = sc.next().charAt(0);
-				
-				//Rohbau CMD bedienelemente
-				if (userInput2 == '1') {
-					System.out.println("Eintrag wird geloescht");
-					System.out.println("");
-					System.out.println("");
-					System.out.print("Beliebige eingabe um zurueck zur Auswahl zu kommen"+" oder "+"Zum verlassen des Programms f druecken");
-					System.out.println("");
-					System.out.println("");
-					char userInputToOutput = sc.next().charAt(0);
-					if(userInputToOutput == 'f') {
-						breakP++;
-					}
-
-				} else if (userInput2 == '2') {
-					System.out.println("Eintrag wird verschoben");
-					System.out.println("");
-					System.out.print("Beliebige eingabe um zurueck zur Auswahl zu kommen"+" oder "+"Zum verlassen des Programms f drueckem");
-					System.out.println("");
-					System.out.println("");
-					char userInputToOutput = sc.next().charAt(0);
-					if(userInputToOutput == 'f') {
-						breakP++;
-					}
-
-				} else if (userInput2 == '3') {
-					System.out.println("");
-					System.out.println("");
-					
-					System.out.println("Eintrag wurde erstellt");
-					System.out.print("Beliebige eingabe um zurueck zur Auswahl zu kommen"+" oder "+"Zum verlassen des Programms f druecken");
-					System.out.println("");
-					System.out.println("");
-					char userInputToOutput = sc.next().charAt(0);
-					if(userInputToOutput == 'f') {
-						breakP++;
-					}
-
-				}
-
-			} else {
-				// Auch nur zum testen
-				System.out.println("Falsch");
-			}
-		} while (breakP <=1 );
-		System.out.println("Wir w�nschen ihnen einen guten Tag");
-
-		sc.close();
+		//User Program interaction:
+		
+		UserInterface user = new UserInterface();
+		user.userInterface();
 	}
 }
