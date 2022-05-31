b1acd2c3d3e13f700de18afcce9149621c481d1a
JGALVEAB <104497110+JGALVEAB@users.noreply.github.com>
2022-06-01 00:20:16 +0200

    Add files via upload

--- Storage.java
+++ Storage.java
@@ -1,46 +1,86 @@
-import java.util.ArrayList;
-import java.util.List;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
 
 public class Storage {
-	
-	public static void storage() {
-//	 try (BufferedReader storagedrive = new BufferedReader(new FileReader(memory));){
-		 int counter =0;
-		 String input;
-		 List <Employee> shiftSchedule = new ArrayList<>();
-		 
-		 
-		
-		 
-//		 while((input = storagedrive.readLine()) != null) {
-//			 if(input.isEmpty()) {
-//				 
-//			 }
-//		 }
-//		 
-//		 
-//	 }catch(IOException e) {
-//		 e.printStackTrace();
-//	 }
-//		  
-	  
-  }
+	String[] list = null;
+
+//	public void storageDrive() {
+//		try (BufferedWriter writer = new BufferedWriter(new FileWriter("txt.txt"));
+//				BufferedReader reader = new BufferedReader(new FileReader(filename));) {
+//
+//			String input = " ";
+//			String[] list = null;
+//
+//			while ((input = reader.readLine()) != null) {
+//				if (!input.isEmpty()) {
+//					list = input.split(";");
+//
+//					try {
+//						String a = list[0];
+//						int b = Integer.parseInt(list[1]);
+//						Employee e = new Employee(a, b);
+//						System.out.println(e);
+//					} catch (NumberFormatException ex) {
+//						System.err.println("Invalid Name or ID");
+//					}
+//
+//				}
+//			}
+//
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//
+//		}
+//	}
+
 	public static void main(String[] args) {
-		storage();
+		Storage ew = new Storage();
+		String filename = "C:\\Users\\gleb-\\Desktop\\shiftschedule\\test.txt";
+		
+		ew.loader(filename);
+		
+	}
+
+	public void loader(String filename) {
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(filename));) {
+			String input = " ";
+			
+
+			while ((input = reader.readLine()) != null) {
+				if (!input.isEmpty()) {
+					list = input.split(";");
+
+					try {
+						String a = list[0];
+						int b = Integer.parseInt(list[1]);
+						Employee e = new Employee(a, b);
+						System.out.println(e);
+					} catch (NumberFormatException ex) {
+						System.err.println("Invalid Name or ID");
+					}
+
+				}
+			}
+		}catch(IOException ioe) {
+			System.out.println("NO File Found!");
+		}
+	}
+	public void saver(String filename, String[] list) {
+		try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename));){
+			String conv = ""; 
+			for(String y : list) {
+				conv += y;
+				char[] a = conv.toCharArray();
+				writer.write(a);
+			}
+		}catch(IOException ioe) {
+			System.out.println("Output failure");
+		}
 	}
 }
-
-/* Verfügbare Mitarbeiter :
-	1. Wade
-	2. Dave
-	3. Seth
-	4. Ivan
-	5. Riley
-	6. Gilbert
-	7. Jorge
-	8. Dan
-	9. Brian
-	10.Roberto
-
-*/
